7db3fa702de4e4ce2832925dec29c69828054b22
Jacob Olin <jolin00@stuy.edu>
2019-04-01 11:37:56 -0400

    Wrote the function getLargest which finds how many digits are in the largest value

--- Radix.java
+++ Radix.java
@@ -1,5 +1,23 @@
 public class Radix{
   public static void radixsort(int[]data) {
-    
+    int[]buckets = new int[20];
+    for (int i = 0;i < getLargest(data);i += 1) {
+
+    }
+  }
+
+  public static int getLargest(int[] data) {
+    int max = 0;
+    for (int i = 0;i < data.length;i += 1) {
+      if (Math.abs(data[i]) >= max) {
+        max = data[i];
+      }
+    }
+    int j = 0;
+    while (max > 0) {
+      max /= 10;
+      j += 1;
+    }
+    return j;
   }
 }
