ba9fc8a06dd4dc6b4d18f2531db0e261361fa258
Jacob Olin <jolin00@stuy.edu>
2019-04-02 11:17:00 -0400

    Now decides which bucket to put each element in

--- Radix.java
+++ Radix.java
@@ -1,5 +1,6 @@
 public class Radix{
   public static void radixsort(int[]data) {
+    @SuppressWarnings({"uncheked","rawtypes"})
     LinkedList<Integer>[]buckets = new LinkedList[20];
     for (int i = 0;i < getLargest(data);i += 1) {
       for (int j = 0;j < buckets.length;j += 1) {
@@ -7,7 +8,10 @@ public class Radix{
       }
       for (int j = 0;j < data.length;j += 1) {
         if (data[j] < 0) {
-          //buckets[9 - ((data[j] / (10**i)) % 10)].add(Math.abs(data[j]));
+          buckets[9 - ((data[j] / (10^i)) % 10)].add(Math.abs(data[j]));
+        }
+        else {
+          buckets[10 + ((data[j] / (10^i)) % 10)].add(Math.abs(data[j]));
         }
       }
 
